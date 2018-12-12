c6e87868c26a7a30bb5e9d166efc82b5f36ad813
iTsHeQ <37482264+iTsHeQ@users.noreply.github.com>
2018-12-12 13:41:26 +0100

    Update p2pchat.erl
    -Funktion hinzugefügt wo man jedem Kontakt ein Ping sendet um zu überprüfen ob sie Online sind
    -erste Methode um jeden Kontakt in meinem Adressbuch zu versenden

--- p2pchat.erl
+++ p2pchat.erl
@@ -1,5 +1,5 @@
 -module(p2pchat).
--export([start_chat/1,start_messenger/2,start_receiver/2]).
+-export([start_chat/1,start_messenger/2,start_receiver/2, readlines/2, get_each_contact/3, ping/2]).
 
 
 start_chat(Prid) ->
@@ -9,9 +9,16 @@ start_chat(Prid) ->
 
 start_messenger(Prid, Receiver) ->
 	Term = io:get_line("You:"),
-	case delete_whitespaces(Term) =:= "/exit\n" of
-		true -> Receiver ! {kill};
-		false ->
+	Test = delete_whitespaces(Term),
+	case Test of
+		"/exit\n" -> Receiver ! {kill};
+		"/C\n" -> 
+			readlines(Prid, Receiver),
+			start_messenger(Prid, Receiver);
+		"/P\n" ->
+			ping(Prid, Receiver),
+			start_messenger(Prid, Receiver);
+		_ ->
 			{chat, Prid} ! {chat, node(), Term},
 			start_messenger(Prid, Receiver)
 	end.
@@ -27,10 +34,61 @@ start_receiver(Prid, Messenger) ->
 			io:format("\~s: ~s", 
                   [lists:nth(1,string:tokens(atom_to_list(PridOfMsg),"@")),Msg]), %converts the Prid to a string, to split it by the @ to only get the username and not the IP-adress
 			start_receiver(Prid, Messenger);
+		{cont, List} ->
+			start_receiver(Prid, Messenger);
+		{ping, Receiver} ->
+			io:format("Ping erhalten ~n"),
+			Receiver ! {pong, Prid},
+			start_receiver(Prid, Messenger);
+		{pong, OnlineContact} ->
+    	io:fwrite("Pong erhalten "),
+		file:write_file("OnlineContact.txt", io_lib:fwrite("~s~n", [OnlineContact]), [append]),
+		start_receiver(Prid, Messenger),
+		start_messenger(Prid, Messenger);
 		{kill} ->
 			io:format("\ Receiver ended ~n")
 
 	end.
+
+readlines(MasterPID, Receiver) ->
+    {ok, Device} = file:open("Contact.txt", [read]),
+    try get_each_contact(Device, MasterPID, Receiver)
+      after file:close(Device)
+    end.
+
+get_each_contact(Device, MasterPID, Receiver) ->
+   case  file:read_line(Device) of
+        {ok, Line} -> 
+
+        	NewList = string:lexemes(Line, [$\n]),
+        	PID = list_to_atom(lists:concat(NewList)),
+        	io:fwrite("Meine Liste: ~p~n", [MasterPID]),
+			{chat, MasterPID} ! {cont, NewList},
+
+      		get_each_contact(Device, MasterPID, Receiver);
+
+        eof        -> start_messenger(MasterPID, Receiver)
+    end.
+
+ping(Prid, Receiver) ->
+	readlinesPing(Prid, Receiver).
+
+readlinesPing(MasterPID, Receiver) ->
+    {ok, Device} = file:open("Contact.txt", [read]),
+    try get_each_contactPing(Device, MasterPID, Receiver)
+      after file:close(Device)
+    end.
+
+get_each_contactPing(Device, User2, Receiver) ->
+   case  file:read_line(Device) of
+        {ok, Line} -> 
+        	NewList = string:lexemes(Line, [$\n]),
+        	PID = list_to_atom(lists:concat(NewList)),
+			{chat, PID} ! {ping, Receiver},
+      		get_each_contactPing(Device, User2, Receiver);
+
+        eof        -> start_messenger(User2, Receiver)
+    end.
 	
 	
 delete_whitespaces(String) -> % does what it says, deletes all whitespaces in a string: "Hello how are you?" -> "Hellohowareyou?" 
